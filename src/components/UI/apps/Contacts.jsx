4c861cb287b8d1bb4de7190b321c40ee572cb6df
Jasper Katalevsky <jyk003@bucknell.edu>
2025-09-29 23:56:29 -0400

    Updated Contacts modal

--- src/components/UI/apps/Contacts.jsx
+++ src/components/UI/apps/Contacts.jsx
@@ -1,11 +1,35 @@
 import './Contacts.css'; 
 import {useState, useEffect} from 'react'; 
 import {createModal, createPortal} from 'react-dom'; 
+import { useApp } from "../../context/AppContext";
+import closeIcon from '../../../assets/icons/Traffic Lights/1-close-1-normal.svg'; 
+import minimizeIcon from "../../../assets/icons/Traffic Lights/2-minimize-1-normal.svg"; 
+import maximizeIcon from "../../../assets/icons/Traffic Lights/3-maximize-1-normal.svg"; 
 
-export function Contacts() {
+export function Contacts({position, onMouseDown}) {
+    const {showContactsModal, setShowContactsModal} = useApp(); 
+
+    const handleModalQuit = (e) => {
+        setShowContactsModal(false); 
+    }
+
     const modal = (
-        <div className="ContactsContainer">
-            Contacts 
+        <div 
+            className="ContactsContainer"
+            style={{
+                position: 'absolute', 
+                left: `${position.x}px`, 
+                top: `${position.y}px`, 
+            }}
+        >   
+            <div className="titlebar" onMouseDown={onMouseDown}>
+                <img className="closeIcon" src={closeIcon} alt="" onClick={handleModalQuit}/>
+                <img src={minimizeIcon} alt="" className="minimizeIcon" />
+                <img src={maximizeIcon} alt="" className="maximizeIcon" />
+            </div>
+            <div className="ContactsMainContainer">
+                <span>Contacts Coming Soon!</span>
+            </div>
         </div>
     )
 
