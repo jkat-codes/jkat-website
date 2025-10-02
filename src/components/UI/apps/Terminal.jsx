4cf43579fd6a5604cdea279bcf1c83873b0709cc
Jasper Katalevsky <jyk003@bucknell.edu>
2025-10-02 12:34:44 -0400

    Added terminal screen on open/reload

--- src/components/UI/apps/Terminal.jsx
+++ src/components/UI/apps/Terminal.jsx
@@ -1,47 +1,44 @@
 import {useState, useEffect} from 'react'; 
 import React from 'react';
-import { createPortal } from "react-dom"; 
+import { createPortal } from 'react-dom'; 
 import { useApp } from '../../context/AppContext'; 
-import "./Projects.css"; 
+import './Terminal.css'; 
 import closeIcon from '../../../assets/icons/Traffic Lights/1-close-1-normal.svg'; 
 import minimizeIcon from "../../../assets/icons/Traffic Lights/2-minimize-1-normal.svg"; 
 import maximizeIcon from "../../../assets/icons/Traffic Lights/3-maximize-1-normal.svg"; 
 
-export function Project({position, onMouseDown}) {
-    const {showProjectsModal, setShowProjectsModal} = useApp(); 
-
+export function Terminal({position, onMouseDown}) {
+    const {showTerminalModal, setShowTerminalModal} = useApp(); 
+    
     const handleModalQuit = (e) => {
-        setShowProjectsModal(false); 
+        setShowTerminalModal(false); 
     }
 
     const modal = (
-        <div 
-            className="ProjectsContainer"
-            style={{
-                position: 'absolute', 
-                left: `${position.x}px`, 
-                top: `${position.y}px`, 
-            }}
-        >   
+        <div className="TerminalContainer" style={{
+            position: 'absolute', 
+            left: `${position.x}px`, 
+            top: `${position.y}px`
+        }}>
             <div className="titlebar" onMouseDown={onMouseDown}>
                 <img className="closeIcon" src={closeIcon} alt="" onClick={handleModalQuit}/>
                 <img src={minimizeIcon} alt="" className="minimizeIcon" />
                 <img src={maximizeIcon} alt="" className="maximizeIcon" />
             </div>
-            <div className="ProjectsMainContainer">
-                <div className="ProjectsSidebar">
-
-                </div>
-                <div className="ProjectsMainContent">
+            <div className="TerminalMainContainer">
+                <div className="TerminalMainContent">
 
                 </div>
             </div>
+
         </div>
     )
+
     const modalRoot = document.getElementById("DesktopContainer"); 
-    if (!modalRoot) {
-        console.log("DesktopContainer not found!"); 
+    if(!modalRoot) {
+        console.log("DesktopContainer not found."); 
         return null; 
     }
+
     return createPortal(modal, modalRoot); 
 }
