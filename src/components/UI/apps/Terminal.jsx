b625675d9f15909cdaf2e26b3a5c2d0e2a2d1586
Jasper Katalevsky <jyk003@bucknell.edu>
2025-10-07 09:35:02 -0400

    Added Terminal text on open, welcome message is logged out to user

--- src/components/UI/apps/Terminal.jsx
+++ src/components/UI/apps/Terminal.jsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react'; 
+import {useState, useEffect, useRef} from 'react'; 
 import React from 'react';
 import { createPortal } from 'react-dom'; 
 import { useApp } from '../../context/AppContext'; 
@@ -7,12 +7,102 @@ import closeIcon from '../../../assets/icons/Traffic Lights/1-close-1-normal.svg
 import minimizeIcon from "../../../assets/icons/Traffic Lights/2-minimize-1-normal.svg"; 
 import maximizeIcon from "../../../assets/icons/Traffic Lights/3-maximize-1-normal.svg"; 
 
+function SequentialTypeWriter({messages}) {
+    // Tracks message index
+    const [currentMessageIndex, setCurrentMessageIndex] = useState(0); 
+    // Tracks character index
+    const [currentCharIndex, setCurrentCharIndex] = useState(0); 
+
+    useEffect(() => {
+        if (currentMessageIndex >= messages.length) return; 
+
+        const currentMessage = messages[currentMessageIndex]; 
+
+        if(currentCharIndex >= currentMessage.length) {
+            const timeout = setTimeout(() => {
+                setCurrentMessageIndex(prev => prev + 1); 
+                setCurrentCharIndex(0); // Reset for next message
+            }, 500); 
+            return () => clearTimeout(timeout); 
+        }
+
+        const interval = setInterval(() => {
+            setCurrentCharIndex(prev => prev + 1); 
+        }, 30); 
+        
+        return () => clearInterval(interval); 
+    }, [currentCharIndex, currentMessageIndex, messages])
+
+    return (
+        <div className="TypewriterContainer">
+            {messages.slice(0, currentMessageIndex + 1).map((message, index) => (
+                index === 0 ? (
+                    <div>
+                        Initializing portfolio...
+                        <br />
+                        <br />
+                    </div>
+                ) : (
+                    index === 1 ? (
+
+                    <div key={index}>
+                            {index < currentMessageIndex ? message: message.substring(0, currentCharIndex)}
+                            <br />
+                            <br />
+                    </div>
+                    ) : (
+                        index === 5 ? (
+                            <div key={index}>
+                                    <br />
+                                    ~/ portfolio (main) % {index < currentMessageIndex ? message: message.substring(0, currentCharIndex)}
+                            </div>
+                        ) : (
+                        <div key={index}>
+                                % {index < currentMessageIndex ? message: message.substring(0, currentCharIndex)}
+                        </div>
+
+                        )
+                    )
+                )
+            ))}
+        </div>
+    )
+}
+
 export function Terminal({position, onMouseDown}) {
     const {showTerminalModal, setShowTerminalModal} = useApp(); 
-    
+    const welcomeString = "hello"; 
+    const [currentMessageIndex, setCurrentMessageIndex] = useState(0); 
+    const stop = useRef(); 
+    const messages = ["", 
+        "Hello! I'm Jasper.", 
+        "Current Focus: ReactJS, Python, SQL, Quantitative Data Analysis", 
+        "Status: Open for work ✅", 
+        "Education: Computer Science and Engineering @ Bucknell University",
+        "Close this window and click the desktop icons to learn more about me"
+
+    ]
+
     const handleModalQuit = (e) => {
         setShowTerminalModal(false); 
     }
+
+    useEffect(() => {
+        stop.current = currentMessageIndex >= welcomeString.length; 
+    }) 
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (stop.current) {
+                clearInterval(interval); 
+            } else {
+                setCurrentMessageIndex((prevIndex) => prevIndex + 1); 
+            }
+        }, 100); 
+        return () => {
+            clearInterval(interval); 
+        }
+    }, []); 
 
     const modal = (
         <div className="TerminalContainer" style={{
@@ -27,7 +117,7 @@ export function Terminal({position, onMouseDown}) {
             </div>
             <div className="TerminalMainContainer">
                 <div className="TerminalMainContent">
-
+                    <SequentialTypeWriter messages={messages}></SequentialTypeWriter>
                 </div>
             </div>
 
